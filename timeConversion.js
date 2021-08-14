c61a6d59d1132fe4a5810728a3aa92a1b78e431b
Ali Raza <alisabri022@gmail.com>
2021-08-14 21:01:24 +0500

    time conversion completed

--- timeConversion.js
+++ timeConversion.js
@@ -1,13 +1,28 @@
-const time1 = "12:05:30AM";
-const time2 = "07:05:45PM";
+const time1 = "07:05:30AM";
+const time2 = "12:45:45PM";
 const timeConvertion = (s) => {
+  let time24;
   if (s.search("PM") != -1) {
     const time = s.replace("PM", "");
     const newTime = time.split(":");
-    const hour = +newTime[0] + 12;
-    console.log(`${hour}:${newTime[1]}:${newTime[2]}`);
+    let hour = +newTime[0] + 12;
+    if (hour == "24") {
+      hour = "12";
+    }
+    time24 = `${hour}:${newTime[1]}:${newTime[2]}`;
+    console.log(time24);
   } else {
-    console.log(s.replace("AM", ""));
+    const time = s.replace("AM", "");
+    const newTime = time.split(":");
+    let hour;
+    if (newTime[0] == "12") {
+      hour = "00";
+    } else {
+      hour = newTime[0];
+    }
+    time24 = `${hour}:${newTime[1]}:${newTime[2]}`;
+    // console.log(`${hour}:${newTime[1]}:${newTime[2]}`);
   }
+  return time24;
 };
 timeConvertion(time2);
